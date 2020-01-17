e1aed4f30a1d98922db145c1764a2ebecafb33e0
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-01-17 20:58:32 +0100

    + steps.ps1

---
+++ command-line/PowerShell/unattended-os-installation/steps.ps1
@@ -0,0 +1,20 @@
+. .\variables.ps1
+. .\remove-vm.ps1
+. .\detect.ps1
+. .\createvm.ps1
+. .\create-hd.ps1
+. .\create-sata.ps1
+. .\create-ide.ps1
+. .\enable-apic.ps1
+#  . .\enable-acpi.ps1    # 
+. .\boot-device-order.ps1
+. .\allocate-memory.ps1
+. .\prepare-shared-folder.ps1
+. .\clipboard-mode.ps1
+. .\vboxsvga.ps1
+. .\install.ps1
+. .\list-vms.ps1
+. .\remove-menues.ps1
+. .\start-vm.ps1
+. .\video-mode-hint.ps1
+. .\wait-for-finished-installation.ps1
