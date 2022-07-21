515c3822f3885b9b6b37171fcb33020521a44471
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-07-21 07:39:30 +0200

    Use variable value for --full-user-name

--- command-line/PowerShell/unattended-os-installation/install.ps1
+++ command-line/PowerShell/unattended-os-installation/install.ps1
@@ -2,7 +2,7 @@ VBoxManage unattended install $vmName      `
   --iso=$isoFile                           `
   --user=$userName                         `
   --password=$password                     `
-  --full-user-name='Tee Queue Eighty-Four' `
+  --full-user-name=$userName               `
   --install-additions                      `
   --time-zone=CET                          `
   --post-install-command='VBoxControl guestproperty set installation_finished y'
