adadf972bd402e2c4204e220ec1b02eddc4dc1a2
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-08-07 17:06:03 +0200

    revert misstakingly deleted line

--- command-line/PowerShell/unattended-os-installation/create-ide.ps1
+++ command-line/PowerShell/unattended-os-installation/create-ide.ps1
@@ -1,2 +1,2 @@
-
-VBoxManage storageattach $vmName --storagectl 'IDE Controller'  --port 0 --device 0 --type dvddrive --medium $isoFile
+VBoxManage storagectl    $vmName --name       'IDE Controller' --add ide
+VBoxManage storageattach $vmName --storagectl 'IDE Controller' --port 0 --device 0 --type dvddrive --medium $isoFile
