a365422668f1599b14f91e9a70217439b2c0e28c
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-08-07 16:44:25 +0200

    Align remarks

--- command-line/PowerShell/unattended-os-installation/variables.ps1
+++ command-line/PowerShell/unattended-os-installation/variables.ps1
@@ -1,10 +1,9 @@
- #
- # Add Virtual Box bin-path to PATH environment variable if necessary:
- #
+#
+# Add Virtual Box bin-path to PATH environment variable if necessary:
+#
 if ( (get-command VBoxManage.exe -errorAction silentlyContinue) -eq $null) {
    $env:path="C:\Program Files\Oracle\VirtualBox;$env:path"
 }
-
 
 $vmName='Win10Preview'
 $vmPath="$home\VirtualBox VMs\$vmName"
