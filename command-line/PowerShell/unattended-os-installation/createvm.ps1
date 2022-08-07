80dee6dc9711d50527930dea4840af59d239ab1e
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-08-07 19:59:31 +0200

    Windows10_64 seems to be the better choice than WindowsNT_64

--- command-line/PowerShell/unattended-os-installation/createvm.ps1
+++ command-line/PowerShell/unattended-os-installation/createvm.ps1
@@ -1,5 +1,5 @@
-# $osType = 'Windows10_64'
-  $osType = 'WindowsNT_64'
+  $osType = 'Windows10_64' # A Windows 10 installation!
+# $osType = 'WindowsNT_64' # An «other» Windows installation
 
 VBoxManage createvm --name $vmName --ostype $osType --register
 
